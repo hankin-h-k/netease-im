629b98422c83b470f0bd1a4ec0318eb90cc42ff8
Hankin <hankin_h@126.com>
2024-10-30 15:17:01 +0800

    踢出群聊

--- team.go
+++ team.go
@@ -12,6 +12,7 @@ const (
 	leaveTam       = neteaseBaseURL + "team/leave.action"          // 主动退群 （高级群）
 	updateTeamNick = neteaseBaseURL + "team/updateTeamNick.action" // 修改群成员昵称 （高级群）
 	removeTeam     = neteaseBaseURL + "team/remove.action"         // 解散群 （高级群）
+	kickTeam       = neteaseBaseURL + "team/kick.action"           // 踢人出群 （高级群）
 
 )
 
@@ -118,6 +119,70 @@ func (c *ImClient) RemoveTeam(tid, owner string) (map[string]*json.RawMessage, e
 	client.SetFormData(param)
 
 	resp, err := client.Post(removeTeam)
+	if err != nil {
+		return nil, err
+	}
+
+	var jsonRes map[string]*json.RawMessage
+	err = jsoniter.Unmarshal(resp.Body(), &jsonRes)
+	if err != nil {
+		return nil, err
+	}
+
+	var code int
+	err = json.Unmarshal(*jsonRes["code"], &code)
+	if err != nil {
+		return nil, err
+	}
+
+	if code != 200 {
+		var msg string
+		json.Unmarshal(*jsonRes["desc"], &msg)
+		return nil, errors.New(msg)
+	}
+
+	return jsonRes, nil
+}
+
+func (c *ImClient) KickBatchTeam(tid, owner, members, attach string) (map[string]*json.RawMessage, error) {
+	param := map[string]string{"tid": tid, "owner": owner, "members": members, "attach": attach}
+	client := c.client.R()
+	c.setCommonHead(client)
+	client.SetFormData(param)
+
+	resp, err := client.Post(kickTeam)
+	if err != nil {
+		return nil, err
+	}
+
+	var jsonRes map[string]*json.RawMessage
+	err = jsoniter.Unmarshal(resp.Body(), &jsonRes)
+	if err != nil {
+		return nil, err
+	}
+
+	var code int
+	err = json.Unmarshal(*jsonRes["code"], &code)
+	if err != nil {
+		return nil, err
+	}
+
+	if code != 200 {
+		var msg string
+		json.Unmarshal(*jsonRes["desc"], &msg)
+		return nil, errors.New(msg)
+	}
+
+	return jsonRes, nil
+}
+
+func (c *ImClient) KickTeam(tid, owner, member, attach string) (map[string]*json.RawMessage, error) {
+	param := map[string]string{"tid": tid, "owner": owner, "member": member, "attach": attach}
+	client := c.client.R()
+	c.setCommonHead(client)
+	client.SetFormData(param)
+
+	resp, err := client.Post(kickTeam)
 	if err != nil {
 		return nil, err
 	}
